e6a4a9e132f97da493eda4dae6646454f9a3448b
ACER <ACER@IMTHANG>
2024-11-12 22:39:22 +0700

    sửa hàm get_likes

--- app/controllers/like_controller.py
+++ app/controllers/like_controller.py
@@ -4,12 +4,14 @@ from app.services.like_service import like_post, unlike_post, get_post_likes
 
 like_bp = Blueprint('like', __name__)
 
+
 @like_bp.route('/<int:post_id>', methods=['POST'])
 @jwt_required()
 def like(post_id):
     user_id = get_jwt_identity()
     result = like_post(user_id, post_id)
     return jsonify(result), result.get('status', 400)
+
 
 @like_bp.route('/<int:post_id>', methods=['DELETE'])
 @jwt_required()
@@ -18,8 +20,10 @@ def unlike(post_id):
     result = unlike_post(user_id, post_id)
     return jsonify(result), result.get('status', 400)
 
-@like_bp.route('/<int:post_id>', methods=['GET'])
-def get_likes(post_id):
-    result = get_post_likes(post_id)
-    return jsonify(result), result.get('status', 400)
 
+@like_bp.route('/<int:post_id>', methods=['GET'])
+@jwt_required()
+def get_likes(post_id):
+    user_id = get_jwt_identity()
+    result = get_post_likes(post_id, user_id)
+    return jsonify(result), result.get('status', 400)
