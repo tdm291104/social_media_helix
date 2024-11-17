0a92df2c9982788a4c6e8fe423a32c382cc6923a
ACER <ACER@IMTHANG>
2024-11-17 17:07:27 +0700

    thêm dữ liệu vào hàm get_post_comments

--- app/services/comment_service.py
+++ app/services/comment_service.py
@@ -1,6 +1,7 @@
 from app.models import db, User, Post, Comment
 from datetime import datetime
 
+
 def create_comment(user_id, data):
     post_id = data.get('post_id')
     content = data.get('content')
@@ -13,11 +14,13 @@ def create_comment(user_id, data):
     if not post:
         return {'message': 'Post not found', 'status': 404}
 
-    comment = Comment(content=content, user_id=user_id, post_id=post_id, created_at=datetime.utcnow())
+    comment = Comment(content=content, user_id=user_id,
+                      post_id=post_id, created_at=datetime.utcnow())
     db.session.add(comment)
     db.session.commit()
 
     return {'message': 'Comment created successfully', 'status': 201}
+
 
 def delete_comment(user_id, comment_id):
     comment = Comment.query.get(comment_id)
@@ -25,42 +28,50 @@ def delete_comment(user_id, comment_id):
         return {'message': 'Comment not found', 'status': 404}
     if comment.user_id != user_id:
         return {'message': 'Permission denied', 'status': 403}
-    
+
     db.session.delete(comment)
     db.session.commit()
 
     return {'message': 'Comment deleted successfully', 'status': 200}
+
 
 def delete_comment_by_admin(comment_id):
     comment = Comment.query.get(comment_id)
     if not comment:
         return {'message': 'Comment not found', 'status': 404}
-    
+
     db.session.delete(comment)
     db.session.commit()
 
     return {'message': 'Comment deleted successfully', 'status': 200}
 
+
 def get_post_comments(post_id):
     post = Post.query.get(post_id)
     if not post:
         return {'message': 'Post not found', 'status': 404}
-    
+
     comments = Comment.query.filter_by(post_id=post_id).all()
     return {
         'comments': [{
             'id': comment.id,
             'content': comment.content,
-            'created_at': comment.created_at
+            'created_at': comment.created_at,
+            'user': {
+                'id': comment.author.id,
+                'username': comment.author.username,
+                'avt': comment.author.media_url
+            }
         } for comment in comments],
         'status': 200
     }
+
 
 def get_user_comments(user_id):
     user = User.query.get(user_id)
     if not user:
         return {'message': 'User not found', 'status': 404}
-    
+
     comments = Comment.query.filter_by(user_id=user_id).all()
     return {
         'comments': [{
@@ -71,16 +82,18 @@ def get_user_comments(user_id):
         'status': 200
     }
 
+
 def get_comment(comment_id):
     comment = Comment.query.get(comment_id)
     if not comment:
         return {'message': 'Comment not found', 'status': 404}
-    
+
     return {
         'id': comment.id,
         'content': comment.content,
         'created_at': comment.created_at
     }
+
 
 def update_comment(user_id, comment_id, data):
     comment = Comment.query.get(comment_id)
@@ -88,7 +101,7 @@ def update_comment(user_id, comment_id, data):
         return {'message': 'Comment not found', 'status': 404}
     if comment.user_id != user_id:
         return {'message': 'Permission denied', 'status': 403}
-    
+
     comment.content = data.get('content')
     db.session.commit()
 
